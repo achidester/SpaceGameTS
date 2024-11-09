2440bf9a6ddb7656a5df027993920709d0c1e3c5
Alex Chidester <chidesta@oregonstate.edu>
2024-11-08 23:39:20 -0800

    event listener for render on movement. commented out for now

--- src/main.ts
+++ src/main.ts
@@ -5,88 +5,84 @@ import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
 // SCENE
-
 const sceneA = new THREE.Scene()
 sceneA.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-const sceneB = new THREE.Scene()
-sceneB.background = new THREE.Color(0x123456)
-const sceneC = new THREE.Scene()
-sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-
 sceneA.add(new THREE.GridHelper())
-let activeScene = sceneA
-const setScene = {
-  sceneA: () => { activeScene = sceneA },
-  sceneB: () => { activeScene = sceneB },
-  sceneC: () => { activeScene = sceneC }
-}
 
 // CAMERA
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+// set camera at positon:
 camera.position.set(0, 2, 3)
 
-const renderer = new THREE.WebGLRenderer()
+
+// RENDERER
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+// document.body.appendChild(renderer.domElement)
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// })
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', () => {
+  renderer.render(sceneA, camera)
 })
-
-
-new OrbitControls(camera, renderer.domElement)
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true, flatShading: true})
+const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
+// OBJECTS
 const cube = new THREE.Mesh(geometry, material)
 cube.position.y = .5
-activeScene.add(cube)
+sceneA.add(cube)
 
+// GUI STATS (FPS)
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+// GUI CONTROLS
 const gui = new GUI()
-const cubeFolder = gui.addFolder("CUBE")
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
-cubeFolder.open()
-
+// GUI - CUBE
+const cubeRotationFolder = gui.addFolder("CUBE rotation")
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2)
+// GUI - CUBE
+const cubeFolder = gui.addFolder("CUBE position")
+cubeFolder.add(cube.position, "x", -5, 5)
+cubeFolder.add(cube.position, "y", -5, 5)
+cubeFolder.add(cube.position, "z", -5, 5)
+// GUI - CAMERA
 const cameraFolder = gui.addFolder("CAMERA")
 cameraFolder.add(camera.position, "x", -20, 20)
 cameraFolder.add(camera.position, "y", 0, 50)
 cameraFolder.add(camera.position, "z", 0, 20)
 
-const sceneFolder = gui.addFolder("SCENE")
-sceneFolder.add(setScene, 'sceneA').name('Scene A')
-sceneFolder.add(setScene, 'sceneB').name('Scene B')
-sceneFolder.add(setScene, 'sceneC').name('Scene C')
-sceneFolder.open()
 
+const clock = new THREE.Clock()
+let delta
 
-
-
+// ANIMATION
 function animate() {
-
   // set framerate
-  setTimeout( function() {
+  // setTimeout(function () {
+  // place animations in here for fps changes
+  // }, 1000 / 100);
 
-    requestAnimationFrame( animate );
+  delta = clock.getDelta()
+  // cube.rotation.y += .4 * delta
+  requestAnimationFrame(animate);
 
-  }, 1000 / 50 );
-
-  
   camera.lookAt(0, 0.5, 0)
-  
-  
-  // cube.rotation.x += 0.001
-  // cube.rotation.y += 0.001 
 
-  renderer.render(activeScene, camera)
+
+  renderer.render(sceneA, camera)
   stats.update()
 }
 
 animate()
+
